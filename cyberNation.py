d706353f5eae4a55fd4b0cd2f546c335f4698b90
Techno Cannon <70115917+TechnoCannon1337@users.noreply.github.com>
2022-04-25 19:42:02 -0700

    Update cyberNation.py

--- cyberNation.py
+++ cyberNation.py
@@ -3,4 +3,4 @@ def cyberNation(CodeOrDIY):
     while True:
         print(CodeOrDIY)
 
-cyberNation('#CYBERNATION: #CODE or #DIY; LIVE BY THE CODE, OR DIY BY THE NODE!')
+cyberNation('#CYBERNATION: #CODE or #DIY; LIVE BY THE CODE, OR DIY BY THE NODE! https://github.com/TechnoCannon1337/Projects/blob/master/cyberNation.py')
