5d49117f5ff4ea731a83bbc9ba3973301d0f8787
Techno Cannon <70115917+TechnoCannon1337@users.noreply.github.com>
2022-04-29 19:33:09 -0700

    Create justfun.py

---
+++ justfun.py
@@ -0,0 +1,16 @@
+#Python
+import time
+from datetime import datetime
+def maximumTwitterFun(LookOutWeekend):
+    dayofTheWeek = datetime.now().isoweekday()
+    while(dayofTheWeek >=5):
+        if dayofTheWeek == 5:
+            day = 'Friday'
+        elif dayofTheWeek == 6:
+            day = 'Saturday'
+        elif dayofTheWeek ==7:
+            day = 'Sunday'
+        print('Today is '+day+' and was made for fun! \n'+LookOutWeekend)
+        time.sleep(86400)
+
+maximumTwitterFun('https://twitter.com/TechnoCannon/status/1520218452329259008')
