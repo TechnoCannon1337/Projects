2b1a7a97514fb098d7db59455283a733cc5777b0
Techno Cannon <70115917+TechnoCannon1337@users.noreply.github.com>
2022-04-30 12:56:47 -0700

    Update maximumfun.py

--- fun/maximumfun.py
+++ fun/maximumfun.py
@@ -5,6 +5,6 @@ def maximumFun(LookOutWeekend):
     day = dayofTheWeek.strftime("%A")
     while day  == 'Friday' or day == 'Saturday' or day == 'Sunday':
         print('Today is '+day+' and was made for fun! \n'+LookOutWeekend)
-        time.sleep(5)
+        time.sleep(86400)
 
 maximumFun('https://github.com/TechnoCannon1338/Projects/raw/master/videos/gotFun.mp4')
