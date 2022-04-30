30e20ba4cd18fe7c79ec58e15e7a3b0e2b5abb69
Techno Cannon <70115917+TechnoCannon1337@users.noreply.github.com>
2022-04-29 21:33:16 -0700

    Create twitterTestFun.py

---
+++ fun/twitterTestFun.py
@@ -0,0 +1,10 @@
+dayofTheWeek = datetime.now().isoweekday()
+while(dayofTheWeek >=5):
+    if dayofTheWeek == 5:
+        day = 'Friday'
+    elif dayofTheWeek == 6:
+        day = 'Saturday'
+    elif dayofTheWeek ==7:
+        day = 'Sunday'
+    print('Today is '+day+' and was made for fun! \n'+LookOutWeekend)
+    time.sleep(86400)
