da007cd7894f015f09ea0e16f25d2ddda3e82e7f
Techno Cannon <70115917+TechnoCannon1337@users.noreply.github.com>
2022-05-03 22:45:53 -0700

    Update PyScriptFunctions.php

--- WordPress/PyScript/PyScriptFunctions.php
+++ WordPress/PyScript/PyScriptFunctions.php
@@ -11,18 +11,7 @@ function pyscript_style()
 	wp_enqueue_style('pyscriptstyle', 'https://pyscript.net/alpha/pyscript.css', array(), false, 'all');
 }
 
-function pyscript_head()
-{
-  ?>
-  <link rel="stylesheet" href="https://pyscript.net/alpha/pyscript.css" />
-  <script defer src="https://pyscript.net/alpha/pyscript.js"></script>
-  <?php
-
-}
-
 
 add_action('wp_enqueue_scripts', 'pyscript_style');
 
 add_action('wp_enqueue_scripts', 'python_pyscript');
-
-add_action( 'wp_head', 'pyscript_head' );
