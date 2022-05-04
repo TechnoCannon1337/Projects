3db85456995586f29f33d21ebcac3c901da98863
Techno Cannon <70115917+TechnoCannon1337@users.noreply.github.com>
2022-05-03 22:47:35 -0700

    Create AddPyScriptFunctionToWordPessHead.php

---
+++ WordPress/PyScript/AddPyScriptFunctionToWordPessHead.php
@@ -0,0 +1,15 @@
+<?php
+
+function pyscript_head()
+{
+  ?>
+  <link rel="stylesheet" href="https://pyscript.net/alpha/pyscript.css" />
+  <script defer src="https://pyscript.net/alpha/pyscript.js"></script>
+  <?php
+
+}
+
+
+add_action( 'wp_head', 'pyscript_head' );
+
+?>
