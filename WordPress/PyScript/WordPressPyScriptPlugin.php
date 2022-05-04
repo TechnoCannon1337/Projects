1480d1080402890790678ccc6bb1c992be94cf9f
Techno Cannon <70115917+TechnoCannon1337@users.noreply.github.com>
2022-05-04 15:03:46 -0700

    Update WordPressPyScriptPlugin.php

--- WordPress/PyScript/WordPressPyScriptPlugin.php
+++ WordPress/PyScript/WordPressPyScriptPlugin.php
@@ -1,1 +1,25 @@
+<?php
+/**
+ * Plugin Name:       WordPress PyScripter
+ * Plugin URI:        https://raw.githubusercontent.com/TechnoCannon1337/Projects/master/WordPress/PyScript/WordPressPyScriptPlugin.php
+ * Description:       The WordPerss PyScripter plug in adds a link to the PyScript stylesheet and JavaScript source file to the head of your WordPress website and also deactivates the wptexturize functions to remove syntax errors resulting from modified quotation marks.
+ * Version:           1.0
+ * Requires at least: 5.93
+ * Requires PHP:      5.93
+ * Author:            Techno Cannon, MPA
+ * Author URI:        https://technocannon.com/github
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://raw.githubusercontent.com/TechnoCannon1337/Projects/master/WordPress/PyScript/WordPressPyScriptPlugin.php
+ */
+function pyscript_head()
+{
+  ?>
+  <link rel="stylesheet" href="https://pyscript.net/alpha/pyscript.css" />
+  <script defer src="https://pyscript.net/alpha/pyscript.js"></script>
+  <?php
 
+add_action( 'wp_head', 'pyscript_head' );
+add_filter('run_wptexturize', '__return_false');
+
+?>
