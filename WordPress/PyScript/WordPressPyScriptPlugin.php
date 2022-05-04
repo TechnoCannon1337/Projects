9d1cdd5baab873a865fd9eccb73ae8a18f9e313c
Techno Cannon <70115917+TechnoCannon1337@users.noreply.github.com>
2022-05-04 15:14:37 -0700

    Update WordPressPyScriptPlugin.php

--- WordPress/PyScript/WordPressPyScriptPlugin.php
+++ WordPress/PyScript/WordPressPyScriptPlugin.php
@@ -18,7 +18,7 @@ function pyscript_head()
   <link rel="stylesheet" href="https://pyscript.net/alpha/pyscript.css" />
   <script defer src="https://pyscript.net/alpha/pyscript.js"></script>
   <?php
-
+}
 add_action( 'wp_head', 'pyscript_head' );
 add_filter('run_wptexturize', '__return_false');
 
