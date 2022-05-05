f8ca7c4fec78c4501d8e021544085d2c62c94b20
Techno Cannon <70115917+TechnoCannon1337@users.noreply.github.com>
2022-05-04 19:19:56 -0700

    Add files via upload

---
+++ WordPress/PyScript/PyScripterPluginforPyScript.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Plugin Name:       PyScripter Plugin for PyScript
+ * Plugin URI:        https://raw.githubusercontent.com/TechnoCannon1337/Projects/master/WordPress/PyScript/WordPressPyScriptPlugin.php
+ * Description:       The PyScripter Plugin for PyScript adds a link to the PyScript stylesheet and JavaScript source file to the head of your WordPress website and also deactivates the wptexturize functions to remove syntax errors resulting from modified quotation marks.
+ * Version:           1.0
+ * Requires at least: 5.9.3
+ * Requires PHP:      7.0
+ * Author:            Techno Cannon, MPA
+ * Author URI:        https://technocannon.com/github
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://raw.githubusercontent.com/TechnoCannon1337/Projects/master/WordPress/PyScript/WordPressPyScriptPlugin.php
+ */
+function pyscript_head()
+{
+  ?>
+  <link rel="stylesheet" href="https://pyscript.net/alpha/pyscript.css" />
+  <script defer src="https://pyscript.net/alpha/pyscript.js"></script>
+  <?php
+}
+
+add_action( 'wp_head', 'pyscript_head' );
+add_filter('run_wptexturize', '__return_false');
+
+?>
