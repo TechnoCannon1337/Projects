edb53b50390f39577507f6a6afcb05b2a21239e7
Techno Cannon <70115917+TechnoCannon1337@users.noreply.github.com>
2022-05-04 19:25:55 -0700

    Update PyScripterPluginforPyScript.php

--- WordPress/PyScript/PyScripterPluginforPyScript.php
+++ WordPress/PyScript/PyScripterPluginforPyScript.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name:       PyScripter Plugin for PyScript
- * Plugin URI:        https://raw.githubusercontent.com/TechnoCannon1337/Projects/master/WordPress/PyScript/WordPressPyScriptPlugin.php
+ * Plugin URI:        https://github.com/TechnoCannon1337/Projects/raw/master/WordPress/PyScript/PyScripterPluginforPyScript.zip
  * Description:       The PyScripter Plugin for PyScript adds a link to the PyScript stylesheet and JavaScript source file to the head of your WordPress website and also deactivates the wptexturize functions to remove syntax errors resulting from modified quotation marks.
  * Version:           1.0
  * Requires at least: 5.9.3
@@ -10,7 +10,7 @@
  * Author URI:        https://technocannon.com/github
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
- * Update URI:        https://raw.githubusercontent.com/TechnoCannon1337/Projects/master/WordPress/PyScript/WordPressPyScriptPlugin.php
+ * Update URI:        https://github.com/TechnoCannon1337/Projects/raw/master/WordPress/PyScript/PyScripterPluginforPyScript.zip
  */
 function pyscript_head()
 {
