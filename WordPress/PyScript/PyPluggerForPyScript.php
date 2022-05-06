5f6c954e61ad119f0fc128930198e3ecda1170bc
Techno Cannon <70115917+TechnoCannon1337@users.noreply.github.com>
2022-05-06 04:11:13 -0700

    Add files via upload
    PyPlugger for PyScript adds a link to the PyScript stylesheet and JavaScript source file to the head and footer of your WordPress website and also deactivates the wptexturize functions to remove syntax errors resulting from modified quotation marks.

---
+++ WordPress/PyScript/PyPluggerForPyScript.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Plugin Name:       PyPlugger for PyScript
+ * Plugin URI:        https://github.com/TechnoCannon1337/Projects/raw/master/WordPress/PyScript/PyPluggerforPyScript.zip
+ * Description:       PyPlugger for PyScript adds a link to the PyScript stylesheet and JavaScript source file to the head and footer of your WordPress website and also deactivates the wptexturize functions to remove syntax errors resulting from modified quotation marks.
+ * Version:           1.0
+ * Requires at least: 5.9.3
+ * Requires PHP:      7.0
+ * Author:            Techno Cannon, MPA
+ * Author URI:        https://technocannon.com/github
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://github.com/TechnoCannon1337/Projects/raw/master/WordPress/PyScript/PyPluggerforPyScript.zip
+ */
+wp_register_script('pyPlugger-Script-ID', 'https://pyscript.net/alpha/pyscript.js', array(), null, true);
+
+wp_register_style('pPlugger-Style-ID', 'https://pyscript.net/alpha/pyscript.css', array(), null);
+
+wp_enqueue_script('pyPlugger-Script-ID', 'https://pyscript.net/alpha/pyscript.js', array(), null, true);
+
+wp_enqueue_style('pPlugger-Style-ID', 'https://pyscript.net/alpha/pyscript.css', array(), null);
+
+add_filter('run_wptexturize', '__return_false');
+
+?>
