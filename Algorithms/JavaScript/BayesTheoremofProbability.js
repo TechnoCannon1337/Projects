79a3a304c9b743f7e856ea459dfbee3b9cf93ef2
Techno Cannon <70115917+TechnoCannon1337@users.noreply.github.com>
2022-05-16 21:39:41 -0700

    Create BayesTheoremofProbability.js

---
+++ Algorithms/JavaScript/BayesTheoremofProbability.js
@@ -0,0 +1,12 @@
+function BayesProbability($A, $B)
+{
+    var $FirstEvent = $A;
+    var $SecondEvent = $B;
+    var $MarginofError = (1-$SecondEvent);
+    var $Unaffected = (1-$FirstEvent);
+    var $Probability = (($FirstEvent*$SecondEvent)/(($FirstEvent*$SecondEvent)+($Unaffected*$MarginofError)));
+    console.log(($FirstEvent*100).toFixed(0) + " percent of the population have been diagnosed for Technophobia using a test with " + ($SecondEvent*100).toFixed(0) + " percent accuracy and a " + ($MarginofError*100).toFixed(0) + " percent margin of error, which means there's a " + ($Probability*100).toFixed(0) + " percent probability that you may also suffer from Tehcnophobia. \n \n  On a positive note, however " + ($Unaffected*100).toFixed(0) + " percent of our Cyber Citizens are most likely to be Source Code Finatics! \n CyberNation: Code or DIY!");
+    return $Probability;
+}
+
+BayesProbability(.05, .9);
