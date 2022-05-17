fec707ffdbda0db5df20ad1c7f5527729dbb1be9
Techno Cannon <70115917+TechnoCannon1337@users.noreply.github.com>
2022-05-16 21:56:15 -0700

    Update BayesTheoremofProbability.py

--- Algorithms/Python/BayesTheoremofProbability.py
+++ Algorithms/Python/BayesTheoremofProbability.py
@@ -5,7 +5,7 @@ def BayesProbability(A, B):
     MarginofError = (1-SecondEvent)
     Unaffected = (1-FirstEvent)
     Probability = ((FirstEvent*SecondEvent)/((FirstEvent*SecondEvent)+(Unaffected*MarginofError)))
-    print(str(round(FirstEvent*100, 0))+ ' percent of the population have been diagnosed for Technophobia using a test with '+str(round(SecondEvent*100, 0))+' percent accuracy and a '+str(round(MarginofError*100, 0))+' percent margin of error, which means there\'s a '+str(round(Probability*100,0))+' percent probability that you may also suffer from Tehcnophobia.\n\n On a positive note, however '+str(round(Unaffected*100, 0))+' percent of our Cyber Citizens are most likely to be Source Code Finatics!\n\n CyberNation: Code or DIY!')
+    print(str(round(FirstEvent*100, 0))+ ' percent of the population have been diagnosed for Technophobia using a test with '+str(round(SecondEvent*100, 0))+' percent accuracy and a '+str(round(MarginofError*100, 0))+' percent margin of error, which means there\'s a '+str(round(Probability*100,0))+' percent probability that you may also suffer from Tehcnophobia.\n\n On a positive note, however '+str(round(Unaffected*100, 0))+' percent of our Cyber Citizens are most likely to be Source Code Fanatics!\n\n CyberNation: Code or DIY!')
     return Probability
 
 BayesProbability(.05, .9)
