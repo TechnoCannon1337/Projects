1ad3734e1df4f0f84dccac3f2b1d880d032df639
Techno Cannon <70115917+TechnoCannon1337@users.noreply.github.com>
2022-05-16 21:55:39 -0700

    Update BayesTheoremofProbability.php

--- Algorithms/PHP/BayesTheoremofProbability.php
+++ Algorithms/PHP/BayesTheoremofProbability.php
@@ -8,7 +8,7 @@ function BayesProbability($A, $B)
     $MarginofError = (1-$SecondEvent);
     $Unaffected = (1-$FirstEvent);
     $Probability = (($FirstEvent*$SecondEvent)/(($FirstEvent*$SecondEvent)+($Unaffected*$MarginofError)));
-    print round($FirstEvent*100, 0, PHP_ROUND_HALF_UP) . " percent of the population have been diagnosed for Technophobia using a test with " . round($SecondEvent*100, 0, PHP_ROUND_HALF_UP) . " percent accuracy and a " . round($MarginofError*100, 0, PHP_ROUND_HALF_UP) . " percent margin of error, which means there's a " . round($Probability*100,0, PHP_ROUND_HALF_UP) ." percent probability that you may also suffer from Tehcnophobia.<p> On a positive note, however " . round($Unaffected*100, 0, PHP_ROUND_HALF_UP) . " percent of our Cyber Citizens are most likely to be Source Code Finatics! <p> CyberNation: Code or DIY!";
+    print round($FirstEvent*100, 0, PHP_ROUND_HALF_UP) . " percent of the population have been diagnosed for Technophobia using a test with " . round($SecondEvent*100, 0, PHP_ROUND_HALF_UP) . " percent accuracy and a " . round($MarginofError*100, 0, PHP_ROUND_HALF_UP) . " percent margin of error, which means there's a " . round($Probability*100,0, PHP_ROUND_HALF_UP) ." percent probability that you may also suffer from Tehcnophobia.<p> On a positive note, however " . round($Unaffected*100, 0, PHP_ROUND_HALF_UP) . " percent of our Cyber Citizens are most likely to be Source Code Fanatics! <p> CyberNation: Code or DIY!";
     return $Probability;
 }
 
