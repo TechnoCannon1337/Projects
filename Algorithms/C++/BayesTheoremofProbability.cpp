74ede23ec2c4f96a78322aeec4ca40cd108eb2d2
Techno Cannon <70115917+TechnoCannon1337@users.noreply.github.com>
2022-05-21 23:45:05 -0700

    Create BayesTheoremofProbability.cpp

---
+++ Algorithms/C++/BayesTheoremofProbability.cpp
@@ -0,0 +1,22 @@
+//Baye's Theorem is used to calculate probailities of events based on prior knowledge
+//add arguments -static-libgcc and -static-libstdc++ to command line when MingW EXE compiler for windows
+#include <iostream>
+#include <cmath>
+using namespace std;
+double BayesProbability(double xA, double xB)
+{
+    double xFirstEvent = xA;
+    double xSecondEvent = xB;
+    double xMarginofError = (1-xSecondEvent);
+    double xUnaffected = (1-xFirstEvent);
+    double xProbability = ((xFirstEvent*xSecondEvent)/((xFirstEvent*xSecondEvent)+(xUnaffected*xMarginofError)));
+    cout << ceil((xFirstEvent*100)) << " percent of the population have been diagnosed for Technophobia using a test with " << ceil((xSecondEvent*100)) << " percent accuracy and a " << ceil((xMarginofError*100)) << " percent margin of error, which means there's a " << ceil((xProbability*100)) << " percent probability that you may also suffer from Tehcnophobia. \n On a positive note, however " << ceil((xUnaffected*100)) << " percent of our Cyber Citizens are most likely to be Source Code Fanatics! \n CyberNation: Code or DIY!" << endl;
+    return xProbability;
+}
+int main()
+{
+int xExit = 0;
+BayesProbability(.05, .9);
+cout << "Press any key and press enter to exit.";
+cin >> xExit;
+}
