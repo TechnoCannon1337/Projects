b13394025950f1f8637afa5f748bd2f7f134a5bb
Techno Cannon <70115917+TechnoCannon1337@users.noreply.github.com>
2023-01-10 17:57:23 -0800

    Update FFMPEGBatchAudioMixer.py

--- BatchProcessing/python/ffmpeg/FFMPEGBatchAudioMixer.py
+++ BatchProcessing/python/ffmpeg/FFMPEGBatchAudioMixer.py
@@ -6,13 +6,13 @@ backGroundMP3File = '/EnterFullDirectoryPathHere/EnterBackgroundSoundFileNameHer
 batchFileDirectory = '/EnterFullDirectoryPathtoBatchofAudioFilesHere/'
 batchDirectoryList = os.listdir(batchFileDirectory)
 primaryFilter = '-filter_complex'
-filterSettings = '[0:a]volume=0.75[a0];[1:a]volume=1.0[a1];[a0][a1]amix=inputs=2:duration=longest[a]'
+filterSettings = '[0:a]volume=-14dB[a0];[1:a]volume=6dB[a1];[a0][a1]amix=inputs=2:duration=longest[a]'
 audioMap = '-map'
 mapSettings ='[a]'
 
 def runAudioMixer():
     for sourceFileName in batchDirectoryList:
         secondInput = os.path.join(batchFileDirectory, sourceFileName)
-        ffCommand = ['ffmpeg', '-i', backGroundMP3File, '-i', secondInput, primaryFilter, filterSettings, audioMap, mapSettings, sourceFileName]
+        ffCommand = ['ffmpeg', '-i', backGroundMP3File, '-i', secondInput, primaryFilter, filterSettings, audioMap, mapSettings, 'mixed_'+sourceFileName]
         subprocess.run(ffCommand)
 runAudioMixer()
